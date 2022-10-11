4ebd1bdd4f9a5cb55b50be7068eb296edab171a4
Nicolas Molina Monroy <hi@nicobytes.com>
2022-10-10 20:08:45 -0500

    save

--- src/exercise.test.js
+++ src/exercise.test.js
@@ -2,7 +2,7 @@ import { sortDesc } from "./exercise";
 
 describe("tests", () => {
   it("should return [4,3,2,1]", () => {
-    const array = [3,1,4,1];
+    const array = [3,1,4,2];
     const rta = sortDesc(array);
     expect(rta).toEqual([4,3,2,1]);
   });
@@ -10,7 +10,7 @@ describe("tests", () => {
   it("should return [-3, -2, -1, 1]", () => {
     const array = [1,-1,-2,-3];
     const rta = sortDesc(array);
-    expect(rta).toEqual([-3, -2, -1, 1]);
+    expect(rta).toEqual([1, -1, -2, -3]);
   });
 
   it("should return []", () => {
